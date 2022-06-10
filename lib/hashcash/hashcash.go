ce16ff9dc7b05fc172fb1c93e2a1900d7adb62da
Augustinas Bacvinka <augustinas.bacvinka@gmail.com>
2022-06-10 14:51:29 +0300

    Added simplified hashcash implementation

---
+++ lib/hashcash/hashcash.go
@@ -0,0 +1,107 @@
+package hashcash
+
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"hash"
+	"math/rand"
+	"strconv"
+)
+
+type Hashcash struct {
+	hasher   hash.Hash
+	bits     uint
+	resource string
+	rand     string
+	counter  uint64
+}
+
+func NewHashcash(resource string, bits uint) *Hashcash {
+	rb, _ := randomBytes(8)
+	return &Hashcash{
+		hasher:   sha1.New(),
+		bits:     bits,
+		resource: resource,
+		rand:     base64EncodeBytes(rb),
+		counter:  rand.Uint64(),
+	}
+}
+
+func (h *Hashcash) Compute() string {
+	header := h.getHeader()
+	for !h.verifyProof(header) {
+		h.counter++
+		header = h.getHeader()
+	}
+	return header
+}
+
+func Verify(header string) (bool, error) {
+	return false, nil
+}
+
+func (h *Hashcash) verifyProof(header string) bool {
+	digest := h.Digest(header)
+	return checkZeros(digest, h.bits)
+}
+
+func (h *Hashcash) Digest(header string) string {
+	h.hasher.Reset()
+	h.hasher.Write([]byte(header))
+	return fmt.Sprintf("%x", h.hasher.Sum(nil))
+}
+
+func (h *Hashcash) getHeader() string {
+	return fmt.Sprintf("1:%d::%s::%s:%s",
+		h.bits,
+		h.resource,
+		h.rand,
+		base64EncodeUint(h.counter),
+	)
+}
+
+func checkZeros(digest string, bits uint) bool {
+	// Do we need additional check for the last rune representing last four bits?
+	mod := bits % 4
+	runes := bits / 4
+	if mod != 0 {
+		runes++
+	}
+
+	for _, v := range digest[:runes-1] {
+		if v != '0' {
+			return false
+		}
+	}
+
+	lastChar := int(digest[runes-1] - '0')
+	if mod == 0 && lastChar != 0 {
+		return false
+	} else if mod == 3 && lastChar > 7 {
+		return false
+	} else if mod == 2 && lastChar > 3 {
+		return false
+	} else if mod == 1 && lastChar > 1 {
+		return false
+	}
+
+	return true
+}
+
+func randomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func base64EncodeBytes(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func base64EncodeUint(n uint64) string {
+	return base64EncodeBytes([]byte(strconv.FormatUint(n, 10)))
+}
---
+++ lib/hashcash/hashcash_test.go
@@ -0,0 +1,63 @@
+package hashcash
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	resource           string
+	bits               uint
+	expectedZeros      int
+	lastCharIdx        int
+	acceptableEndChars string
+}
+
+var commonCases = []testCase{
+	{"test1", 1, 0, 0, "01"},
+	{"test2", 2, 0, 0, "0123"},
+	{"test3", 3, 0, 0, "01234567"},
+	{"test4", 4, 1, 0, "0"},
+	{"test5", 11, 2, 2, "01234567"},
+	{"test6", 13, 2, 2, "01"},
+	{"test7", 14, 2, 2, "0123"},
+}
+
+func TestGetHeader(t *testing.T) {
+	tc := commonCases[0]
+	hash := NewHashcash(tc.resource, tc.bits)
+	emptyHeader := hash.getHeader()
+	proofHeader := hash.Compute()
+
+	for _, header := range []string{emptyHeader, proofHeader} {
+		values := strings.Split(header, ":")
+		assert.Equal(t, len(values), 7, "header should contain 7 fields seperated by ':'")
+		assert.Equal(t, values[1], fmt.Sprintf("%d", tc.bits), fmt.Sprintf("%s should be %d", values[1], tc.bits))
+		assert.Equal(t, values[3], tc.resource, fmt.Sprintf("%s should be %s", values[3], tc.resource))
+	}
+}
+
+func TestCheckZeros(t *testing.T) {
+	for _, tc := range commonCases {
+		hash := NewHashcash(tc.resource, tc.bits)
+		proof := hash.Compute()
+		digest := hash.Digest(proof)
+
+		// check expected zeros
+		if tc.expectedZeros > 0 {
+			zeros := "0"
+			for i := 1; i < tc.expectedZeros; i++ {
+				zeros = zeros + "0"
+			}
+			assert.True(t, digest[:tc.expectedZeros] == zeros, fmt.Sprintf("header should have %s as a suffix", zeros))
+		}
+
+		// check last char
+		lastChar := string(digest[tc.lastCharIdx])
+		assert.True(t, strings.Contains(tc.acceptableEndChars, lastChar),
+			fmt.Sprintf("end char is '%s' but should be one of %s", lastChar, tc.acceptableEndChars))
+	}
+}
