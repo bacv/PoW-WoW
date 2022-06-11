0cf139eece0e495a5e26ac0dce96003326d51df0
Augustinas Bacvinka <augustinas.bacvinka@gmail.com>
2022-06-11 17:36:26 +0300

    Added reconnect flag

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -17,19 +17,27 @@ func main() {
 	log.SetOutput(os.Stdout)
 	rand.Seed(time.Now().UnixNano())
 	addr := flag.String("addr", ":8080", "address to receive words of wisdom from")
+	reconn := flag.Bool("reconn", false, "try to reconnect until it succeeds")
 	flag.Parse()
 
 	wowService := wow.NewWowClientService()
 
-	conn, err := net.Dial("tcp", *addr)
-	if err != nil {
-		log.Fatal(err)
+	for {
+
+		conn, err := net.Dial("tcp", *addr)
+		if err != nil {
+			if *reconn {
+				continue
+			}
+			log.Fatal(err)
+		}
+		transport := svc.NewTransport(conn, wowService.Handle)
+
+		go func() {
+			transport.Write(protocol.NewRequestMsg())
+		}()
+
+		transport.Spawn()
+		break
 	}
-	transport := svc.NewTransport(conn, wowService.Handle)
-
-	go func() {
-		transport.Write(protocol.NewRequestMsg())
-	}()
-
-	transport.Spawn()
 }
